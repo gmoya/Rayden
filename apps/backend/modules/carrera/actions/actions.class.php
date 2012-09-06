dd64e954558f7b23ca7ad60aa2a3f9899ba4cbda
GustavoMoya <gustavo22@hotmail.com>
2012-09-06 00:50:58 -0300

    Aplicación backend generada. Módulo de Carrera generado.

---
+++ apps/backend/modules/carrera/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/carreraGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/carreraGeneratorHelper.class.php';
+
+/**
+ * carrera actions.
+ *
+ * @package    rayden
+ * @subpackage carrera
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class carreraActions extends autoCarreraActions
+{
+}
---
+++ apps/backend/modules/carrera/lib/carreraGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * carrera module helper.
+ *
+ * @package    rayden
+ * @subpackage carrera
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class carreraGeneratorHelper extends BaseCarreraGeneratorHelper
+{
+}
