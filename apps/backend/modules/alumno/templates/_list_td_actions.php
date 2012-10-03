247412a42421e0e00c5f01a3e2e65547d85d6d09
GustavoMoya <gustavo22@hotmail.com>
2012-10-03 10:39:04 -0300

    Se actualiza la url del icono editar, que estaba mal escrita.

--- apps/backend/modules/alumno/templates/_list_td_actions.php
+++ apps/backend/modules/alumno/templates/_list_td_actions.php
@@ -1,10 +1,10 @@
 <td>
   <ul class="sf_admin_td_actions">
 		<li class="sf_admin_action_edit">
-			<a class="icon edit colorbox" title="<?php echo __('Edit') ?>" href="<?php echo url_for('/alumno/'.$Alumno->getId().'/edit') ?>"> </a>
+			<a class="icon edit colorbox" title="<?php echo __('Edit') ?>" href="<?php echo url_for('alumno/edit?id='.$Alumno->getId()) ?>"> </a>
 		</li>
 		<li class="sf_admin_action_delete">
-			<a class="icon delete colorbox" title="<?php echo __('Delete') ?>" href="<?php echo url_for('/alumno/delete?id='.$Alumno->getId()) ?>"> </a>
+			<a class="icon delete colorbox" title="<?php echo __('Baja') ?>" href="<?php echo url_for('alumno/baja?id='.$Alumno->getId()) ?>"> </a>
 		</li>
   </ul>
 </td>
